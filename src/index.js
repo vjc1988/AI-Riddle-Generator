f111e8118980c3e78739a726017ecac76b21c04b
vjc1988 <victoriajaynecadd@yahoo.com>
2024-05-05 21:21:08 +0100

    Added the AI API integration

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
-function generateRiddle(event) {
-  event.preventDefault();
-
+function generateRiddle(response) {
   new Typewriter("#riddle-result", {
-    strings: "riddle results",
+    strings: response.data.answer,
     autoStart: true,
     delay: 40,
     cursor: "",
   });
 }
 
+function riddleGenerator(event) {
+  event.preventDefault();
+
+  let userInstructions = document.querySelector("#userInstructions");
+  let apiKey = "bd69aeefb72b8a36of7aa0db00f9b34t";
+  let prompt = `display a difficult and short riddle on the subject only provided by ${userInstructions.value}. provide the answer to the riddle seperated from the riddle by a <br /><br />. Please display the ridde as follows Riddle: Answer: both in a <strong> element`;
+  let context =
+    "you are a playful riddle AI assistant who enjoys giving users a challenge";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(generateRiddle);
+}
+
 let riddleInputElement = document.querySelector("#submit-riddle");
-riddleInputElement.addEventListener("submit", generateRiddle);
+riddleInputElement.addEventListener("submit", riddleGenerator);
