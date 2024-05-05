0908eb901fc1c23aaa01e57206c6266d94c8a1c7
vjc1988 <victoriajaynecadd@yahoo.com>
2024-05-05 20:29:49 +0100

    Added basic form handling and added Typewriter Plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateRiddle(event) {
+  event.preventDefault();
 
+  new Typewriter("#riddle-result", {
+    strings: "riddle results",
+    autoStart: true,
+    delay: 40,
+    cursor: "",
+  });
+}
+
+let riddleInputElement = document.querySelector("#submit-riddle");
+riddleInputElement.addEventListener("submit", generateRiddle);
