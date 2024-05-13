48fcd9647a04ba8058d36ab8e2bd63a930d03aee
vjc1988 <victoriajaynecadd@yahoo.com>
2024-05-13 18:33:35 +0100

    Changed colour scheme and improved the user experience

--- src/index.js
+++ src/index.js
@@ -12,13 +12,13 @@ function riddleGenerator(event) {
 
   let userInstructions = document.querySelector("#userInstructions");
   let apiKey = "bd69aeefb72b8a36of7aa0db00f9b34t";
-  let prompt = `display a difficult and short riddle on the subject only provided by ${userInstructions.value}. provide the answer to the riddle seperated from the riddle by a <br /><br />. Please display the ridde as follows Riddle: Answer: both in a <strong> element`;
-  let context =
-    "you are a playful riddle AI assistant who enjoys giving users a challenge";
+  let prompt = `display a fun short riddle on the subject only provided by ${userInstructions.value}. Provide the answer to the riddle seperated from the riddle by a <br /><br />. Please display the ridde as follows Riddle: Answer: both in a <strong> element`;
+  let context = `you are a playful riddle AI assistant who enjoys giving users a challenging riddle about the subect provided by ${userInstructions.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let riddleElement = document.querySelector("#riddle-result");
   riddleElement.innerHTML = `<div class="blink">⌛ Generating a riddle about ${userInstructions.value}...</div>`;
+  riddleElement.classList.remove("hidden");
 
   axios.get(apiUrl).then(generateRiddle);
 }
