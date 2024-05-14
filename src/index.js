a57d6cdfce033e7de9ca49c2cb253c8ecb9ffbe4
vjc1988 <victoriajaynecadd@yahoo.com>
2024-05-14 13:19:49 +0100

    Specified to Animal Riddle Generator/ updated AI prompt and context

--- src/index.js
+++ src/index.js
@@ -12,12 +12,12 @@ function riddleGenerator(event) {
 
   let userInstructions = document.querySelector("#userInstructions");
   let apiKey = "bd69aeefb72b8a36of7aa0db00f9b34t";
-  let prompt = `display a fun short riddle on the subject only provided by ${userInstructions.value}. Provide the answer to the riddle seperated from the riddle by a <br /><br />. Please display the ridde as follows Riddle: Answer: both in a <strong> element`;
-  let context = `you are a playful riddle AI assistant who enjoys giving users a challenging riddle about the subect provided by ${userInstructions.value}`;
+  let prompt = `display a fun short riddle about ${userInstructions.value} only no other animal or answer. Provide the answer to the riddle seperated from the riddle by a <br /><br />. Please display the ridde as follows Riddle: Answer: both in a <strong> element`;
+  let context = `you are a playful riddle AI assistant who enjoys giving users a challenging riddle about the an animal provided by ${userInstructions.value} only`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let riddleElement = document.querySelector("#riddle-result");
-  riddleElement.innerHTML = `<div class="blink">⌛ Generating a riddle about ${userInstructions.value}...</div>`;
+  riddleElement.innerHTML = `<div class="blink">⌛ Generating a riddle about a ${userInstructions.value}...</div>`;
   riddleElement.classList.remove("hidden");
 
   axios.get(apiUrl).then(generateRiddle);
